e18b126ed449fe7bf43e473176be8d42db42ba23
mohitdubey01 <mohitdubey42551@gmail.com>
2024-11-02 15:16:27 +0530

    Modified After Cloning

--- Launch.java
+++ Launch.java
@@ -2,5 +2,11 @@ class Launch
 {
 public static void main(String[] args){
 System.out.println("Welcome");
+public void add(){
+int a= 9;
+int b = 8;
+int sum  = a+b;
+System.out.println(sum);
+}
 }
 }
